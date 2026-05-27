b70ca49685303c0a0753e15b279f0cb4009567c3
dhakalavanish-afk <dhakalavanish@gmail.com>
2026-05-27 06:54:51 -0400

    Update leads.js

--- leads.js
+++ leads.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const supabase = require('./config/supabase');
+const supabase = require('./supabase');
 
 router.post('/', async (req, res) => {
 try {
